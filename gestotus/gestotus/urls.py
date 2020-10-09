ba0ecc606400a840d88f65648ffc98277b17e815
Jonathan Mieldazis <mieldazis@ilhasoft.com.br>
2020-10-09 14:06:48 -0300

    add gerenciamento de conteudos

--- gestotus/gestotus/urls.py
+++ gestotus/gestotus/urls.py
@@ -13,12 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from django.views.generic import TemplateView
+
+from .portal_publico.views import IndexPortalPublicoView, SobrePortalPublicoView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", TemplateView.as_view(template_name="index.html"), name="index"),
-    path("about/", TemplateView.as_view(template_name="about.html"), name="about")
-]
+    path("", IndexPortalPublicoView.as_view(), name="index"),
+    path("sobre/", SobrePortalPublicoView.as_view(), name="about"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
