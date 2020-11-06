37faedc37496a2826740c2ee3d0e18437f99c6af
Jonathan Mieldazis <mieldazis@ilhasoft.com.br>
2020-11-06 01:06:42 -0300

    add list and create link dr licitacao

--- gestotus/gestotus/urls.py
+++ gestotus/gestotus/urls.py
@@ -3,14 +3,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from django.views.generic import TemplateView
-
+from .drlicitacao.views import DrLicitacaoListView
 from .portal_publico.views import IndexPortalPublicoView, SobrePortalPublicoView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", IndexPortalPublicoView.as_view(), name="index"),
     path("sobre/", SobrePortalPublicoView.as_view(), name="about"),
-    path("drlicitacao/", TemplateView.as_view(template_name="drlicitacao.html"), name="drlicitacao"),
+    path("drlicitacao/", DrLicitacaoListView.as_view(), name="drlicitacao"),
     path("artigos/", include("gestotus.blog.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
