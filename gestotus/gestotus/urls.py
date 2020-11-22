db6412e0ca40621135315adfbcb71b046556f796
pedrobarbosa0 <47837747+pedrobarbosa0@users.noreply.github.com>
2020-11-21 23:08:03 -0300

    cursos list

--- gestotus/gestotus/urls.py
+++ gestotus/gestotus/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 
 from .drlicitacao.views import DrLicitacaoListView
 from .portal_publico.views import IndexPortalPublicoView, SobrePortalPublicoView
+from .cursos.views import
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,4 +13,5 @@ urlpatterns = [
     path("sobre/", SobrePortalPublicoView.as_view(), name="about"),
     path("drlicitacao/", DrLicitacaoListView.as_view(), name="drlicitacao"),
     path("artigos/", include("gestotus.blog.urls")),
+    path("cursos/",)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
